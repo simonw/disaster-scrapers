f8cb70d5e08665b79ab53bce72f2d4a211777da4
Simon Willison <swillison@gmail.com>
2019-06-10 20:32:15 -0700

    Scrape all of the CA fires

--- cal_fires.py
+++ cal_fires.py
@@ -2,24 +2,35 @@ from disaster_scrapers import Scraper
 from bs4 import BeautifulSoup as Soup
 import requests
 
-# Just doing Sand Fire at the moment
 
-class SandFire(Scraper):
-    url = "http://www.fire.ca.gov/current_incidents/incidentdetails/Index/2322"
+class FireScraper(Scraper):
     owner = "simonw"
     repo = "disaster-data"
-    filepath = "cal-fires/2322-sand-fire.json"
+
+    def __init__(self, token, incident_id, name):
+        self.incident_id = incident_id
+        self.filepath = "cal-fires/{}-{}.json".format(
+            self.incident_id, name.lower().strip().replace(" ", "-")
+        )
+        super().__init__(token)
 
     def fetch_data(self):
-        soup = Soup(requests.get(self.url).content, "html5lib")
+        soup = Soup(
+            requests.get(
+                "http://www.fire.ca.gov/current_incidents/incidentdetails/Index/{}".format(
+                    self.incident_id
+                )
+            ).content,
+            "html5lib",
+        )
         table = soup.select(".incident_table")[0]
-        info = {
-            "summary": table["summary"],
-        }
+        info = {"summary": table["summary"]}
         for tr in table.select("tr"):
             try:
                 title = tr.select("td.emphasized")[0]
-                info[title.text.strip().rstrip(":")] = title.find_next("td").text.strip()
+                info[title.text.strip().rstrip(":")] = title.find_next(
+                    "td"
+                ).text.strip()
             except:
                 pass
         # Split out latitude and longitude, if available
@@ -28,3 +39,28 @@ class SandFire(Scraper):
             info["latitude"] = latitude
             info["longitude"] = longitude
         return info
+
+
+def load_scrapers(token):
+    # Scrape the list of fires
+    url = "http://www.fire.ca.gov/current_incidents/"
+    page = 1
+    incidents = []  # List of (id, name) tuples
+    while True:
+        html = requests.get("{}?page={}".format(url, page)).text
+        soup = Soup(html, "html5lib")
+        scraped_incidents = [
+            (t.get("id"), t.get("title"))
+            for t in soup.select("table.incident_table")
+            if t.get("id")
+        ]
+        if not scraped_incidents:
+            break
+        incidents.extend(scraped_incidents)
+        page += 1
+        if "?page={}".format(page) not in html:
+            break
+        if page > 100:
+            break
+    # Return a scraper for each incident
+    return [FireScraper(token, incident_id, name) for incident_id, name in incidents]
--- scrape_all.py
+++ scrape_all.py
@@ -4,23 +4,30 @@ import importlib
 import os
 
 
-def discover_scrapers():
+def discover_scrapers(token):
     scrapers = []
     for filepath in pathlib.Path(".").glob("*.py"):
         mod = importlib.import_module(filepath.stem)
-        for klass in mod.__dict__.values():
-            try:
-                if issubclass(klass, Scraper) and klass.__module__ != "disaster_scrapers":
-                    scrapers.append(klass)
-            except TypeError:
-                pass
+        # if there's a load_scrapers() function, call that
+        if hasattr(mod, "load_scrapers"):
+            scrapers.extend(mod.load_scrapers(token))
+        # Otherwise instantiate a scraper for each class
+        else:
+            for klass in mod.__dict__.values():
+                try:
+                    if (
+                        issubclass(klass, Scraper)
+                        and klass.__module__ != "disaster_scrapers"
+                    ):
+                        scrapers.append(klass(token))
+                except TypeError:
+                    pass
     return scrapers
 
 
 if __name__ == "__main__":
     github_token = os.environ.get("GITHUB_TOKEN")
-    for scraper_class in discover_scrapers():
-        scraper = scraper_class(github_token)
+    for scraper in discover_scrapers(github_token):
         if github_token is None:
             scraper.test_mode = True
         scraper.scrape_and_store()
