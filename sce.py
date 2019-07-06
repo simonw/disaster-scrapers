eb3b37319a397b0f9b3d4af255453bf874a30354
Simon Willison <swillison@gmail.com>
2019-07-06 06:56:24 -0700

    Track outages reported by Southern California Edison
    There are earthquakes in Southern California right now.
    
    Will be written to https://github.com/simonw/sce-outages

---
+++ sce.py
@@ -0,0 +1,36 @@
+from disaster_scrapers import DeltaScraper
+import requests
+
+
+class SCEOutages(DeltaScraper):
+    url = "https://prodms.dms.sce.com/outage/v1/power/outage"
+    owner = "simonw"
+    repo = "sce-outages"
+    filepath = "sce-outages.json"
+
+    record_key = "incidentId"
+    noun = "incident"
+
+    def fetch_data(self):
+        data = requests.get(self.url, timeout=10).json()
+        return data["outageMapDataResponse"]["AOCIncidents"]["incident"]
+
+    def display_record(self, outage):
+        display = []
+        display.append(
+            "  {incidentId} in {cityName} {countyName} affecting {numberOfCustomersAffected}".format(
+                outage
+            )
+        )
+        display.append(
+            "    https://www.google.com/maps/search/{centroidY},{centroidX}".format(
+                outage
+            )
+        )
+        display.append(
+            "    {memoCauseCodeDescription} - {crewStatusCodeDescription}".format(
+                outage
+            )
+        )
+        display.append("")
+        return "\n".join(display)
